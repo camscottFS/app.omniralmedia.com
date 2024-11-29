8571c640edfbbe366feed2ee1a10d738792be79f
camscotthbi <126517545+camscotthbi@users.noreply.github.com>
2024-11-29 16:29:45 -0600

    Update Modal.tsx

--- frontend/src/components/modal/Modal.tsx
+++ frontend/src/components/modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 interface ModalProps {
   title: string;
@@ -7,9 +7,20 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ title, children, setModalOpen }) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      setModalOpen(false);
+    }
+  };
+
   return (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white rounded-lg shadow-lg p-6 w-80">
+    <div
+      className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50"
+      onClick={handleBackdropClick}
+    >
+      <div ref={modalRef} className="bg-white rounded-lg shadow-lg p-6 w-80">
         <h2 className="text-lg text-blue-900 font-semibold mb-4">
           {title}
         </h2>
@@ -26,7 +37,7 @@ const Modal: React.FC<ModalProps> = ({ title, children, setModalOpen }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Modal;
